0c80c6bc7b97b2c9f518d914a0636d10f840c747
surajkumarsingh1784 <167981827+surajkumarsingh1784@users.noreply.github.com>
2025-06-29 02:05:50 +0530

    add images for seller login imp

--- client/src/components/seller/SellerLogin.jsx
+++ client/src/components/seller/SellerLogin.jsx
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import { useAppContext } from '../../context/AppContext'
+import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import toast from 'react-hot-toast'
+import MegaMart from '../../assets/MegaMart.png'
 
 const SellerLogin = () => {
-    const{isSeller,setIsSeller,navigate} = useAppContext()
+    const { isSeller, setIsSeller } = useAppContext()
+    const navigate = useNavigate()
     const[email,setEmail]= useState('')
     const[password,setPassword]= useState('')
 
-    useEffect(()=>{
-        if(isSeller){
-            navigate('/seller')
-        }
-    },[isSeller])
+    useEffect(() => {
+        if (isSeller) navigate('/seller')
+    }, [isSeller, navigate])
     
     const onSubmitHandler = async (e) => {
         try{
@@ -32,24 +33,23 @@ const SellerLogin = () => {
     }
 
   return !isSeller && (
-    <form onSubmit={onSubmitHandler} className='min-h-screen flex items-center text-sm text-gray-500'>
-      <div className='flex flex-col gap-5 items-start p-8 py-12 min-w-80 sm:min-w-88 m-auto border border-gray-300 rounded-lg shadow-xl'>
-      <p className='text-2xl font-medium m-auto'><span className=' text-primary-dull'> Seller</span> Login</p>
-      <div className='w-full'>
-        <p>Email</p>
-        <input onChange={(e)=>setEmail(e.target.value)} value={email}
-        type='text' placeholder='Enter Email' className='border border-gray-300 rounded w-full p-2 mt-1 outline-primary'required/>
-      </div>
-      <div className='w-full'>
-        <p>Password</p>
-        <input onChange={(e)=>setPassword(e.target.value)} value={password}
-        type='password' placeholder='Enter Password'className='border border-gray-300 rounded w-full p-2 mt-1 outline-primary'required/>
-      </div>
-      <button className=' bg-primary text-white w-full py-2 rounded-md cursor-pointer'>Login Seller</button>
-      
-      
-      </div>
-    </form>
+    <div className='min-h-screen flex items-center text-sm text-gray-600 relative'>
+      <img src={MegaMart} alt='MegaMart Logo' className='h-12 fixed top-4 left-4 cursor-pointer' onClick={() => navigate('/')} />
+      <form onSubmit={onSubmitHandler} className='flex flex-col gap-5 m-auto items-start p-8 py-12 min-w-80 sm:min-w-88 rounded-lg shadow-xl border border-gray-200'>
+        <p className='text-2xl font-medium m-auto'><span className='text-primary'>Seller</span> Login</p>
+        <div className='w-full'>
+          <p>Email</p>
+          <input onChange={(e)=>setEmail(e.target.value)} value={email}
+          type='text' placeholder='Enter Email' className='border border-gray-300 rounded w-full p-2 mt-1 outline-primary'required/>
+        </div>
+        <div className='w-full'>
+          <p>Password</p>
+          <input onChange={(e)=>setPassword(e.target.value)} value={password}
+          type='password' placeholder='Enter Password'className='border border-gray-300 rounded w-full p-2 mt-1 outline-primary'required/>
+        </div>
+        <button className=' bg-primary text-white w-full py-2 rounded-md cursor-pointer'>Login Seller</button>
+      </form>
+    </div>
   )
 }
 
