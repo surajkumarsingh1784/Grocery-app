7c2e70b7b098a9a03818cc9156482496883d82d0
surajkumarsingh1784 <167981827+surajkumarsingh1784@users.noreply.github.com>
2025-07-01 15:55:09 +0530

    j

--- server/server.js
+++ server/server.js
@@ -24,19 +24,13 @@ const allowedOrigins = [
   ].filter(Boolean);
 
   app.use(cors({
-    origin: function (origin, callback) {
-      console.log('CORS Request from origin:', origin);
-      console.log('Allowed origins:', allowedOrigins);
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        console.log('CORS blocked for origin:', origin);
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5174', 
+      'http://localhost:5175',
+      'https://grocery-app-orpin.vercel.app'
+    ],
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
   }));
   
 
