152b2db6ede77a5a9a4bc1ec388948543ea95d49
surajkumarsingh1784 <167981827+surajkumarsingh1784@users.noreply.github.com>
2025-07-07 13:52:21 +0530

    ba

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -72,7 +72,7 @@ export const login = async (req, res) => {
             maxAge: 7 * 24 * 60 * 60 * 1000 // cookie expiration time
         })
 
-        return res.json({success: true, message: 'User logged in  successfully', user : {email: user.email, name: user.name}});
+        return res.json({success: true, message: 'User logged in  successfully', user : {_id: user._id, email: user.email, name: user.name}});
     }catch(error){
         console.log(error.message);
         res.json({success: false, message: error.message});
